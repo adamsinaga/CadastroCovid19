94ef24ca159c958d51b47df44c9f407d30ca66e3
adamsinaga <38689336+adamsinaga@users.noreply.github.com>
2023-06-20 18:41:22 -0400

    Add files via upload

---
+++ src/main/java/edu/ifmt/cadastrovacinacovid/CadastrovacinacovidApplication.java
@@ -0,0 +1,13 @@
+package edu.ifmt.cadastrovacinacovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrovacinacovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrovacinacovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifmt/cadastrovacinacovid/CadastrovacinacovidApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifmt.cadastrovacinacovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrovacinacovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
